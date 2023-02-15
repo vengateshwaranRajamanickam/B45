b8118e57a189f7f10c323469b8a5068768979293
vengateshwaran S R <vengateshwaran.1994@gmail.com>
2023-02-15 22:54:10 +0530

    day 2

--- fruit.js
+++ fruit.js
@@ -1,16 +1,23 @@
+
+
 var fruit=[
-{name:"apple",color:"red"}
-{name:"banana",color:"yellow"}
+{name:"apple" , color:"red"},
+{name:"banana", color:"yellow"}
 ]
 
 let der=document.createElement("div");
+der.innerHTML="<p>test</P>";
 document.body.appendChild(der);
-
-for (let i-0;i<fruit.length;i++){
-    let fru=document.createElement("p");
-    fru.innerText=fru[i].name;
-    fru.setAttribute("style",+person[i].color);
-}
+let fru=document.createElement("p");
+//for (let i=0;i<=fruit.length;i++){
+   // fru.innerText=fruit[i].color;
+   // fru.setAttribute("style","color:red");
+//}
 der.appendChild(fru);
-
-
+// go to conole area
+console.log(document.getElementById('head'));
+// printin html area
+let k=document.getElementById('head');
+document.body.appendChild(k);
+console.log(document.URL);
+alert("hacked");
