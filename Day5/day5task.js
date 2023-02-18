0cb77238628b02c6ed7f2b3e34e942e963b3333b
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-19 02:09:32 +0530

    palindrome

--- Day5/day5task.js
+++ Day5/day5task.js
@@ -1,8 +1,7 @@
+/*
 let arr=[324,555,667,882,907];
-
 let dis=document.createElement("p");
 document.body.appendChild(dis);
-
 //print out the odd number in an array
 let odd=[];
 arr.forEach((value)=>{
@@ -22,29 +21,11 @@ function caps(arr){
     console.log(u.toUpperCase(),...rest);
 }
 // sum of all numbers in an array
-/*n
 let sum=0;
 arr.forEach((value)=>{
     sum+=parseInt(value);
 });
 console.log(sum);
-//rotate an array by k times
-
-var rotate=["king ",234,"joy","lion"];
-var time= 5;
-//for (let i=1;i<=time;i++){
-change(rotate);
-function change(value){
-    for (let i=0,j=1;i<3;i++,j++){
-       
-    let length=(rotate.length-j);
-    let swap=value[length];
-    value[length]=value[i];
-    value[i]=swap;
-    //console.log(...value);
-        //console.log(...value);
-    }
-}*/
 //find prime number of array
 let prime=[332,7,9,17,44];
 let primenumber=[];
@@ -61,6 +42,41 @@ for(let j=0;j<prime.length;j++){
     primenumber.push(prime[j]);
 }
 console.log(...primenumber);
+*/
+//return all palindromes in array
+let palindromes=["motor","apple","mango","radar"];
+let ans=[];
+var k=0;
+palindromes.forEach((value)=>{
+    if(value.length/2!==0){
+    for(let i=0;i<value.length/2;i++){
+    for(let j=1;j<=value.length/2;j--){
+    if(value[i]===value[j])
+    k+=0
+    }
+    }
+    if(k===value.length/2)
+    ans.push(value);
+    }
+});
+console.log(ans);
+/*
+//rotate an array by k times
+var rotate=["king ",234,"joy","lion"];
+var time= 5;
+//for (let i=1;i<=time;i++){
+change(rotate);
+function change(value){
+    for (let i=0,j=1;i<3;i++,j++){
+       
+    let length=(rotate.length-j);
+    let swap=value[length];
+    value[length]=value[i];
+    value[i]=swap;
+    //console.log(...value);
+        //console.log(...value);
+    }
+}
 //remove duplicate from array
 let duplicate=[332,7,7,17,44,7];
 let answer=[];
@@ -75,4 +91,4 @@ for(let j=0;j<duplicate.length;j++){
     answer.push(duplicate[j]);
 }
 console.log(...answer);
-//
+*/
