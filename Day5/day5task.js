90b4d7ac2b5f069a45561294a51ce1e21fce71cf
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-20 00:47:31 +0530

    day5 task

--- Day5/day5task.js
+++ Day5/day5task.js
@@ -1,4 +1,4 @@
-/*
+
 let arr=[324,555,667,882,907];
 let dis=document.createElement("p");
 document.body.appendChild(dis);
@@ -10,7 +10,6 @@ arr.forEach((value)=>{
     }
 });
 console.log(...odd);
-
 //all string to title caps in an array
 let fruits=["apple","orange","mango"];
 let v;
@@ -42,7 +41,6 @@ for(let j=0;j<prime.length;j++){
     primenumber.push(prime[j]);
 }
 console.log(...primenumber);
-
 //return all palindromes in array
 let palindromes=["motor","apple","mango","radar"];
 let ans=[];
@@ -58,45 +56,45 @@ palindromes.forEach((value)=>{
 console.log(palindromes);
 
 //return median of two sorted array of same size
-//let median1=[90,89,79,877,55];
-//let median2=[12,26,29,99,43];
 const median=(...arr)=>{
     mid = Math.floor(arr.length/2);
     num=[...arr].sort((a,b)=>a-b);
     return (num.length% 2!==0) ? num[mid]: (num[mid]+num[mid-1])/2;
-  }
-  console.log(median(10,20,30,50,100,80));
-  console.log(median(80,90,120,110,269,786));
-
+}
+console.log(median(90,89,79,877,55));
+console.log(median(80,90,120,110,269,786));
 
 //rotate an array by k times
-var rotate=["king ",234,"joy","lion"];
-var time= 5;
-//for (let i=1;i<=time;i++){
-change(rotate);
-function change(value){
-    for (let i=0,j=1;i<3;i++,j++){
-       
-    let length=(rotate.length-j);
-    let swap=value[length];
-    value[length]=value[i];
-    value[i]=swap;
-    //console.log(...value);
-        //console.log(...value);
+    const kt = function ktimes(k,...value){
+    const temp=value;
+    value=[];
+    for (let i=0;i<temp.length;i++){
+    let p = (i+k) % temp.length ;
+    value[p]=temp[i]; 
     }
+    return value;
 }
-*/
+console.log(kt(5,89,234,80,33));
+
 //remove duplicate from array
-let duplicate=[332,7,7,17,44,7];
-let answer=[];
-for(let j=0;j<duplicate.length;j++){
-    let ans=0;
-     for(let i=0;i<duplicate.length;i++){
-            if(duplicate[j]===duplicate[i] && i!==j)
-            ans=1;
-            break;
-       }
-    if(ans===1)
-    [duplicate[j]].push();
-}
-console.log(answer);
+let duplicate=[332,78,7,7,44,7];
+const tofindduplicate= (value)=>value.filter((item,index)=>value.indexOf(item)===index)
+console.log(tofindduplicate(duplicate));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
