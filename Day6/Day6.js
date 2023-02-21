6aa3241fd3a452436a366fe469f2b6f1fb7443f3
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-22 00:49:55 +0530

    22/2

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -35,6 +35,15 @@ console.log(student2.totalmarks());
 console.log(student3.totalmarks());
 console.log(student4.totalmarks());
 console.log(student5.totalmarks());
+//child class
+class rank extends person{
+    constructor(name,rollnumber,maths,physics,chemistry,rank){
+    super(name,rollnumber,maths,physics,chemistry);
+    this.rank=rank;
+    }
+}
+rank.prototype.award="school first";
+
 //class for uber fare
 class uber{
     constructor(name,email,mobile,otp,uberpoint,tripid){
@@ -64,10 +73,12 @@ this.tripid=tripid;
     }
 }
 let ubercustomer1=new uber("revi","revi@a,in",01234567,1234,50,2);
-console.log(ubercustomer1.startjourney());
-// console.log(ubercustomer1.endjourney());
-console.log(ubercustomer1.Totalfair()); */
-// class for movie of PG value
+//console.log(ubercustomer1.startjourney());
+//console.log(ubercustomer1.endjourney());
+console.log(ubercustomer1.Totalfair()); 
+*/
+
+//class for movie of PG value
 class movie{
     constructor(title,studio,rating){
 this.title=title;
@@ -75,27 +86,63 @@ this.studio=studio;
 this.rating=rating;
     }
     PGonly(){
-        let com=this.rating.split(" ");
-        if(com[1]>=0);
-        return this.rating;
+        if(this.rating==="PG")
+        return this.title;
     }
     instant(){
-        let a=prompt("enter movie name");
-        let b=prompt("enter production house name");
-        let c=prompt("enter rating");
-        return `${a}movie by ${b} is rated as ${c} `
+        let inst =new Object();
+        inst.title=prompt("enter movie name");
+        inst.studio=prompt("enter production house name");
+        inst.rating=prompt("enter rating");
+        return inst
     }
 
     
 }
 let Movie1= new movie("robo","sun picture","PG 13");
-let Movie2= new movie("2.0","lyca","PG 15");
+let Movie2= new movie("2.0","lyca","PG");
 let Movie3= new movie("love today","ags","PG 18");
-let Movie4= new movie("","","");
+let Movie4=new Object();
+Movie4= new movie("","","");
 let findPGmovie=[];
-findPGmovie[0]=Movie1.PGonly();
-findPGmovie[1]=Movie2.PGonly();
-findPGmovie[2]=Movie3.PGonly();
-findPGmovie[3]=Movie4.PGonly();
-console.log(...findPGmovie);
+findPGmovie[1]=Movie1.PGonly();
+findPGmovie[2]=Movie2.PGonly();
+findPGmovie[3]=Movie3.PGonly();
+findPGmovie[4]=Movie4.PGonly();
+console.log(findPGmovie);
 //console.log(Movie4.instant());
+
+
+/*
+//prototype to class for add variable and function
+class uberdemo{
+    constructor(name){
+    this.name=name;
+    }    
+}
+let customer3=new uberdemo("li");
+uberdemo.prototype.sex="m";                 //adding value in object class
+uberdemo.prototype.age=function(){          //adding function to the object class
+    let date= new Date;
+    return date.getFullYear() -this.birth;
+}
+customer3.birth=1993;
+console.log(customer3.sex);
+console.log(customer3.age());
+
+//prototype for asyn function
+Array.prototype.firstElement=function (){
+    return this[0];
+}
+let test1=new Array();
+test1=[34,55,32]
+console.log(test1.firstElement([34,55,32]));
+
+//prototype for arrow function
+Array.prototype.firstElement=(value)=>{
+    return value[0];
+};
+let test=new Array();//[7,9,0];
+console.log(test.firstElement([34,55,32]));
+
+*/
---
+++ Day7/Day7.js
@@ -0,0 +1,62 @@
+// let userdata=[{"createdAt":"2023-02-20T10:11:04.420Z","name":"Eloise Raynor","mobileNumber":"1-914-349-7458 x805","email":"Norma.Hilpert@example.com","age":45,"id":"8"},{"createdAt":"2023-02-20T17:50:12.535Z","name":"Ruth Barton","mobileNumber":"530-645-5981 x317","email":"Tyrese_Ziemann89@example.net","age":41,"id":"9"},{"createdAt":"2023-02-20T21:59:05.658Z","name":"Elsa Wolff","mobileNumber":"891-863-1890","email":"Darryl_Cassin@example.net","age":55,"id":"10"},{"createdAt":"2023-02-20T14:30:56.246Z","name":"Reginald Gerlach I","mobileNumber":"1-905-928-2957 x5772","email":"Solon_Schaden@example.org","age":17,"id":"11"},{"createdAt":"2023-02-20T13:31:18.833Z","name":"Cynthia Rohan","mobileNumber":"(656) 990-7076 x3797","email":"Eriberto.Johnston@example.net","age":99,"id":"12"},{"createdAt":"2023-02-21T01:01:28.964Z","name":"Annie Koch","mobileNumber":"1-260-877-4215 x779","email":"Easton_Padberg@example.net","age":70,"id":"13"},{"createdAt":"2023-02-20T10:39:02.224Z","name":"Leigh Rosenbaum","mobileNumber":"(653) 674-8099 x04997","email":"Derrick.Von@example.org","age":86,"id":"14"},{"createdAt":"2023-02-20T21:40:15.082Z","name":"Kerry Hintz","mobileNumber":"1-306-513-8806","email":"Dallin.Kemmer@example.org","age":7,"id":"15"},{"createdAt":"2023-02-20T11:58:15.629Z","name":"Joseph Ankunding","mobileNumber":"1-501-718-8515","email":"Moriah95@example.org","age":31,"id":"16"}];
+// let d=JSON.parse(userdata);
+// let b=userdata.map((value)=>{
+//     if(value.age>18)
+//     return `${value.name} is eligible ${value.age}`;
+
+
+
+// });
+
+//console.log(userdata.b());
+
+// const number=0;
+// const ansmten=mten([3,4,5,2]);
+// function mten(num){
+//     return num[0]*10;
+// }
+// console.log(ansmten);
+
+let testo = [{ "createdAt": "2023-02-20T10:11:04.420Z", "name": "Eloise Raynor", "mobileNumber": "1-914-349-7458 x805", "email": "Norma.Hilpert@example.com", "age": 45, "id": "8" }, 
+{ "createdAt": "2023-02-20T17:50:12.535Z", "name": "Ruth Barton", "mobileNumber": "530-645-5981 x317", "email": "Tyrese_Ziemann89@example.net", "age": 41, "id": "9" },
+{ "createdAt": "2023-02-20T21:59:05.658Z", "name": "Elsa Wolff", "mobileNumber": "891-863-1890", "email": "Darryl_Cassin@example.net", "age": 55, "id": "10" }, 
+{ "createdAt": "2023-02-20T14:30:56.246Z", "name": "Reginald Gerlach I", "mobileNumber": "1-905-928-2957 x5772", "email": "Solon_Schaden@example.org", "age": 17, "id": "11" }, { "createdAt": "2023-02-20T13:31:18.833Z", "name": "Cynthia Rohan", "mobileNumber": "(656) 990-7076 x3797", "email": "Eriberto.Johnston@example.net", "age": 99, "id": "12" }, { "createdAt": "2023-02-21T01:01:28.964Z", "name": "Annie Koch", "mobileNumber": "1-260-877-4215 x779", "email": "Easton_Padberg@example.net", "age": 70, "id": "13" }, 
+{ "createdAt": "2023-02-20T10:39:02.224Z", "name": "Leigh Rosenbaum", "mobileNumber": "(653) 674-8099 x04997", "email": "Derrick.Von@example.org", "age": 86, "id": "14" }, 
+{ "createdAt": "2023-02-20T21:40:15.082Z", "name": "Kerry Hintz", "mobileNumber": "1-306-513-8806", "email": "Dallin.Kemmer@example.org", "age": 7, "id": "15" }, { "createdAt": "2023-02-20T11:58:15.629Z", "name": "Joseph Ankunding", "mobileNumber": "1-501-718-8515", "email": "Moriah95@example.org", "age": 31, "id": "16" },
+{ "createdAt": "2023-02-20T20:04:32.103Z", "name": "Dominick Turner", "mobileNumber": "454.283.1636 x8366", "email": "Imani.Will10@example.com", "age": 87, "id": "17" }]
+//console.log(testo);
+const f=testo.map(value=>{
+    let ans= new Object();
+    if(value.age>18){ 
+    ans.valid="eligible";
+    ans.name=value.name;
+    }
+    else {
+    ans.valid="not eligible"
+    ans.name=value.name;
+    }
+    return {ans}
+})
+console.log(f);
+
+const k=testo.forEach(value=>{
+    value.mobileNumber;
+})
+console.log(k);
+
+const g=testo.map(value=>{
+    let total=value.age+5;   //.map method and using object shortcuthand
+    return {total};
+    
+})
+console.log(...g);
+
+const m=testo.map(value=>{
+    let mn=value.age;          //.map method and using object shortcuthand
+    return [mn];
+})
+console.log(m);
+
+const senior=testo.filter(({age})=>age>60)   //sorting senior citizen age >60
+console.log(senior);
+ 
