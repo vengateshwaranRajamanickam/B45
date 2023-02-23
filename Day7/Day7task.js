65e4b16c596f6dfc558fe329c249f64723f28659
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-23 15:52:47 +0530

    commit

--- Day7/Day7task.js
+++ Day7/Day7task.js
@@ -8,7 +8,7 @@ document.body.appendChild(der);
 xho.onload=function (){
     user=JSON.parse(xho.response);
     console.log(user);
-    let ans1,ans2 = new Array;
+    let ans1,ans2,ans3,ans4 = new Array;
     ans1=user.filter(({continents})=>continents=="Asia");
     ans2=user.filter(({population})=>population<200000);
     ans1=ans1.map((value)=>{
@@ -16,12 +16,24 @@ xho.onload=function (){
         sortregion=value.capital;
         return sortregion;
     });
+    // ans3=user.filter(({currencies})=>{
+    //     //let currenciesid=new Array();
+    //     let {symbol}=currencies;
+    //     console.log(symbol);
+        //return currenciesid
+    });
+    // ans4=user.forEach(({name, capital, flag})=>{
+    //     console.log(name.common);
+    //     console.log(capital);
+
+
     console.log(ans1);
     console.log(ans2);
+    console.log(ans3);
     // ans2=user.filter((value)=>{
     // if((value.population)<200000)
     // return value.countries ;       
-    // });
+    // });name, capital, flag
     //ans3=user.filter(({USdollar,countries})=>population<2000000?countries:"");
 //console.log(user.grabdetail());
 }
