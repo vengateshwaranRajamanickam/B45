6290aeee43308828f5a5b1e55cc054f304a3b28d
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-23 23:21:27 +0530

    FINISH

--- Day7/Day7task.js
+++ Day7/Day7task.js
@@ -7,42 +7,22 @@ let der=document.createElement("p");
 document.body.appendChild(der);
 xho.onload=function (){
     user=JSON.parse(xho.response);
-    console.log(user);
-    let ans1,ans2,ans3,ans4 = new Array;
+    let ans1,ans2,ans3,findusd = new Array;
     ans1=user.filter(({continents})=>continents=="Asia");
+    ans1=ans1.map((value)=>value.capital);
     ans2=user.filter(({population})=>population<200000);
-    ans1=ans1.map((value)=>{
-        let sortregion=[];
-        sortregion=value.capital;
-        return sortregion;
-    });
-    ans3=user.filter(({currencies})=>{
-        //let currenciesid=new Array();
-        let {symbol}=currencies;
-        console.log(symbol);
-        return currenciesid
-    });
+    ans3=user.map(({currencies})=>currencies.hasOwnProperty("USD"));
     ans4=user.forEach(({name, capital, flags})=>{
         let img=new Object();
         console.log(name.common);
-        console.log(...capital);
-           let {png}=flags;
-           img.src=png;
-           console.log(png);  
+        console.log(capital);
+        let {png}=flags;
+        img.src=png;
+        console.log(png);  
     });
 
     console.log(ans1);
     console.log(ans2);
     console.log(ans3);
-    console.log(ans4);
-    // ans2=user.filter((value)=>{
-    // if((value.population)<200000)
-    // return value.countries ;       
-    // });name, capital, flag
-    //ans3=user.filter(({USdollar,countries})=>population<2000000?countries:"");
-//console.log(user.grabdetail());
+   
 }
-    
-
-
-
