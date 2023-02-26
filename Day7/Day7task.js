d4668e33e8ec54283994c23981d6faecf7d67a4c
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-02-26 10:54:46 +0530

    task update

--- Day7/Day7task.js
+++ Day7/Day7task.js
@@ -3,15 +3,22 @@ let user=new Array;
 xho.open("GET","https://restcountries.com/v3.1/all");
 xho.send();
 
-let der=document.createElement("p");
-document.body.appendChild(der);
+// let der=document.createElement("p");
+// document.body.appendChild(der);
+// let current={voltage:{name:"resistor",reading:"time"},element:{name:"resist",reading:"t"}}
+// if((current.voltage).hasOwnproperty(`name`)){
+// let {voltage}=current;
+// console.log(voltage);}
+
+
 xho.onload=function (){
     user=JSON.parse(xho.response);
-    let ans1,ans2,ans3,findusd = new Array;
+    let ans1,ans2,ans3 = new Array;
+    let findusd=new Object;
     ans1=user.filter(({continents})=>continents=="Asia");
     ans1=ans1.map((value)=>value.capital);
     ans2=user.filter(({population})=>population<200000);
-    ans3=user.map(({currencies})=>currencies.hasOwnProperty("USD"));
+     ans3=user.filter(({currencies})=>currencies?.USD);
     ans4=user.forEach(({name, capital, flags})=>{
         let img=new Object();
         console.log(name.common);
