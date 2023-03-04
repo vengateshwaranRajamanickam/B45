83db4e058b33ddf4849793d55b99d3e93daf11c6
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-03-04 15:10:20 +0530

    change

--- Day12/2/2.js
+++
@@ -1,5 +0,0 @@
-const qselector = ()=>document.querySelector(".Testing").style.color="red"
-const qselectorall = ()=>{
-    let ans=document.querySelectorAll(".Testing");
-    ans.forEach((value)=>value.style.color="green")
-};
