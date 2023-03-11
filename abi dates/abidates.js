b940564d211f82c5245e9196e9c8e3d1d457e3e8
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-03-11 14:33:57 +0530

    update

---
+++ abi dates/abidates.js
@@ -0,0 +1,15 @@
+let img= new Image(100,100);
+img.src="./abidates image/offer.jpg";
+// setTimeout(()=>{
+//     document.getElementById(offer).
+// },5000)
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
