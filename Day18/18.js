50cc0b5183182b32ae213e1120d9085f80036719
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-03-12 21:02:32 +0530

    u

--- Day18/18.js
+++ Day18/18.js
@@ -1,14 +1,32 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    var g=document.getElementsById(mySelect);
-    fetch("https://restcountries.com/v2/all").then(data=>data.json())
-    //.then(res=>console.log(res))
-    .then(res=>{
-      let out="";
-      res.forEach(countries => out+=`<option>${countries.names}</option>`)
-      console.log(out);
-      g.innerHTML=out;
-    })
-     });
+//document.addEventListener('DOMContentLoaded', () => {
+
+  //const selectDrop = document.querySelector('#countries');
+  const selectDrop = document.getElementById('countries');
+
+
+  fetch('https://restcountries.com/v2/all').then(res=>{
+    return res.json();
+  }).then(data=> 
+    {
+    let outpu = "";
+    data.forEach(value =>outpu += `<option value="${value.name}">${value.name}</option>`)
+    selectDrop.innerHTML = outpu;
+  })
+ 
+//});
+// document.addEventListener('DOMContentLoaded',()=>{
+//   const selectDrop=document.querySelector("countries")
+//   fetch("https://restcountries.com/v2/all").then((res)=>{res.json()
+//     .then((data)=>{
+//       let output="";
+//       data.forEach(({country}) => {
+//         output+=`<option>${country.names}</option>`
+//       })
+//       selectDrop.innerHTML=output;
+//     })
+//   })
+//     .catch((err)=>console.log(err))
+//     });
       
     // fetch("https://openweathermap.org")
     // .then((data)=>data.json())
