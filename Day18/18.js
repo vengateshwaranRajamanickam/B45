d4c97eda3180c01cbedaab5108d6960acb9fa38d
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-03-12 23:27:56 +0530

    u

--- Day18/18.js
+++ Day18/18.js
@@ -1,23 +1,38 @@
 //document.addEventListener('DOMContentLoaded', () => {
 
   //const selectDrop = document.querySelector('#countries');
-  const selectDrop = document.getElementById('countries');
-
+  const selectDrop = document.getElementById('selectcountries');
+  const xcap = document.getElementById('xcapital');
+  const xreg = document.getElementById('xregion');
+  var lib;
 
   fetch('https://restcountries.com/v2/all').then(res=>{
     return res.json();
   }).then(data=> 
     {
+    lib=data;
     let outpu = "";
     data.forEach(value =>outpu += `<option value="${value.name}">${value.name}</option>`)
     selectDrop.innerHTML = outpu;
   })
  
 //});
-      
-    fetch("https://openweathermap.org")
-    .then((res)=>res.json())
-    .then((data)=>console.log(data))
+function myFunction() {
+  var x = document.getElementById("selectcountries").value;
+  xcap.innerText= x;
+  console.log(lib);
+  lib.forEach(value =>{
+  if(value.name==x){
+    xreg.innerText= value.region;
+   
+  }
+})
+
+}
+    
+    // fetch("https://openweathermap.org")
+    // .then((res)=>res.json())
+    // .then((data)=>console.log(data))
     //document.body.getElementsByClassName(listregion).value.continents)
     // let g=document.getElementById("mylist");
     // function parameter(e){
